bb933671c8d714ec3d580742cfad343c1dfb3d89
Kelson da Costa Medeiros <kelson.medeiros@lais.huol.ufrn.br>
2022-10-26 20:07:29 -0300

    feat: add: protótipo do serviço que retorna as notas em JSON

--- sync_down_grades.php
+++ sync_down_grades.php
@@ -3,22 +3,38 @@ require_once('../../config.php');
 require_once(__DIR__.'/locallib.php');
 
 
-function suap_sync_down_grades() {
-    global $CFG;
+class sync_up_enrolments_service extends service {
 
-    try { 
-        suap_sync_authenticate();
+    function call() {
+        global $CFG, $DB;
+    
+        try { 
+            $this->authenticate();
 
-        dienow("Não implementado.", 501);
-    } catch (Exception $ex) {
-        http_response_code(500);
-        if ($ex->getMessage() == "Data submitted is invalid (value: Data submitted is invalid)") {
-            echo json_encode(["error" => ["message" => "Ocorreu uma inconsistência no servidor do AVA. Este erro é conhecido e a solução dele já está sendo estudado pela equipe de desenvolvimento. Favor tentar novamente em 5 minutos."]]);
-        } else {
-            echo json_encode(["error" => ["message" => $ex->getMessage()]]);
+            $notas = $DB->get_record_sql('
+                select   gi.idnumber codigo_nota, u.username matricula_aluno, gg.finalgrade nota_aluno
+                from     {grade_grades} gg
+                            inner join {grade_grades} gi on (gg.itemid=gi.id)
+                            inner join {grade_grades} c on (gi.courseid = c.id)
+                            inner join {user} u on (gg.userid=u.id)
+                where    gi.idnumber is not null and gi.idnumber != ''
+                and    c.idnumber = ?;
+            ', [$_GET['codigo_diario']]);
+
+            echo json_encode($notas);
+        } catch (Exception $ex) {
+            http_response_code(500);
+            if ($ex->getMessage() == "Data submitted is invalid (value: Data submitted is invalid)") {
+                echo json_encode(["error" => ["message" => "Ocorreu uma inconsistência no servidor do AVA. Este erro é conhecido e a solução dele já está sendo estudado pela equipe de desenvolvimento. Favor tentar novamente em 5 minutos."]]);
+            } else {
+                echo json_encode(["error" => ["message" => $ex->getMessage()]]);
+            }
         }
     }
+
 }
 
 
-suap_sync_down_grades();
+$service = new sync_up_enrolments_service();
+$service->call();
+    
