445ae76211a92964800e215a1c9a6d327040a192
Kelson da Costa Medeiros <kelson.medeiros@lais.huol.ufrn.br>
2023-02-07 18:54:40 -0300

    feat: fix: login após logout com problema

--- login.php
+++ login.php
@@ -1,4 +1,12 @@
 <?php
 require_once('../../config.php');
 
-redirect("$CFG->wwwroot/auth/oauth2/login.php?id=1&wantsurl=$CFG->wwwroot&sesskey={$_SESSION['USER']->sesskey}");
+if (property_exists($_SESSION['USER'], 'sesskey')) {
+    $wantsurl = "$CFG->wwwroot{$_SERVER['REQUEST_URI']}";
+    $sesskey = $_SESSION['USER']->sesskey;
+    redirect("$CFG->wwwroot/auth/oauth2/login.php?id=1&sesskey=$sesskey&wantsurl=$wantsurl");
+} else {
+    // require_once "../../login/logout.php";
+    redirect("$CFG->wwwroot/auth/oauth2/login.php?errorcode=4&id=1&sesskey=$sesskey&wantsurl=$wantsurl");
+    echo "Sessão inválida. Por favor, <a href='$CFG->wwwroot'>tente novamente</a>.";
+}
