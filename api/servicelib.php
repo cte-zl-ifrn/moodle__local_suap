9439eea543fe6c1a7fa7b17e20890809743220ff
Kelson da Costa Medeiros <kelson.medeiros@lais.huol.ufrn.br>
2023-02-17 18:58:12 -0300

    build: reorganizando os código

---
+++ api/servicelib.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * SUAP Integration
+ *
+ * This module provides extensive analytics on a platform of choice
+ * Currently support Google Analytics and Piwik
+ *
+ * @package     local_suap
+ * @category    upgrade
+ * @copyright   2020 Kelson Medeiros <kelsoncm@gmail.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_suap;
+
+
+class service {
+
+    function authenticate() {
+        $sync_up_auth_token = config('auth_token');
+
+        $headers = getallheaders();
+        if (!array_key_exists('Authentication', $headers)) {
+            dienow("Bad Request - Authentication not informed", 400);
+        }
+
+        if ("Token $sync_up_auth_token" != $headers['Authentication']) {
+            dienow("Unauthorized", 401);
+        }
+    }
+
+    function call() {
+        try { 
+            $this->authenticate();
+            echo json_encode($this->do_call());
+        } catch (Exception $ex) {
+            http_response_code(500);
+            if ($ex->getMessage() == "Data submitted is invalid (value: Data submitted is invalid)") {
+                echo json_encode(["error" => ["message" => "Ocorreu uma inconsistência no servidor do AVA. Este erro é conhecido e a solução dele já está sendo estudado pela equipe de desenvolvimento. Favor tentar novamente em 5 minutos."]]);
+            } else {
+                echo json_encode(["error" => ["message" => $ex->getMessage()]]);
+            }
+        }
+    }
+
+}
