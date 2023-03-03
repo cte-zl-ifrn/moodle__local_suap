f641896c4201bb8a6e29189c2099354cb28334d1
CTE ZL/IFRN <cte.ead@ifrn.edu.br>
2023-03-03 11:51:01 -0300

    feat: add: healthcheck.php

---
+++ healthcheck.php
@@ -0,0 +1,7 @@
+<?php
+require_once("../../config.php");
+echo "<pre>PHP: OK.\n
+Configuração do Moodle: OK.\n
+Conexão com o banco: OK.\n
+Rota no .htaccess: OK.\n
+Tudo bem até aqui:" . " sim.\n";
