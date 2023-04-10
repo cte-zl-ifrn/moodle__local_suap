f13b00b5f5c4c3a9053a6c93e1283c9c9a1a8477
CTE ZL/IFRN <cte.ead@ifrn.edu.br>
2023-04-10 10:30:07 -0300

    feat: [fix] caso o usuário não exista na base informa isso e retorna a API no padrão

--- api/get_atualizacoes_counts.php
+++ api/get_atualizacoes_counts.php
@@ -12,7 +12,15 @@ class get_messages_service extends \local_suap\service{
         global $DB, $USER;
 
         $USER = $DB->get_record('user', ['username' => $_GET['username']]);
-        return $this->get_atualizacoes_counts($USER->id);
+        if ($USER) {
+            return $this->get_atualizacoes_counts($USER->id);
+        } else {
+            return [
+                'error' => ['message' => "Usuário '{$_GET['username']}' não existe", 'code' => '404'],
+                'unread_conversations_count' => 0,
+                'unread_popup_notification_count' => 0,
+            ];
+        }
     }
 
     function get_atualizacoes_counts($useridto) {
