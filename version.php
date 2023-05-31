b48768faef62014698695232c93283fef3aa6847
Kelson da Costa Medeiros <kelson.medeiros@lais.huol.ufrn.br>
2023-05-31 11:09:25 -0300

    env: [fix] em alguns cenários o version não era atribuído

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_suap';
 $plugin->release = '0.2.027';
-$plugin->version = 20231900000 + substr($plugin->release, 4);
+$plugin->version = 20231900027;
 $plugin->maturity = MATURITY_RC;
 $plugin->requires = 2021051700;
