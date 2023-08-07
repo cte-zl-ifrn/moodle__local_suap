51c0b7ac1331782956d0cfe0cad9c5fc12e6488d
Ramon de Oliveira Araújo Mateus <60753390+Ramon-Mateus@users.noreply.github.com>
2023-08-07 17:51:21 -0300

    SQL que retorna as notas dos alunos de um curso passado por query string

--- api/sync_down_grades.php
+++ api/sync_down_grades.php
@@ -9,22 +9,35 @@ require_once('../../../config.php');
 require_once("../locallib.php");
 require_once("servicelib.php");
 
+// Link de acesso (exemplo): http://ava/local/suap/api/sync_down_grades.php?codigo_diario=20231.1.15806.1E.TEC.1386
 
 class sync_up_enrolments_service extends service {
-
+    
     function call() {
         global $CFG, $DB;
-        // $this->authenticate();
+        
+        try { 
+            $this->authenticate();
+            
+            $notas = $DB->get_records_sql("
+                select   gi.id id, gi.idnumber codigo_nota, u.username matricula_aluno, gg.finalgrade nota_aluno
+                from     mdl_grade_grades gg
+                            inner join mdl_grade_items gi on (gg.itemid=gi.id)
+                            inner join mdl_course c on (gi.courseid = c.id)
+                            inner join mdl_user u on (gg.userid=u.id)
+                where    gi.idnumber is not null and gi.idnumber != ''
+                and    c.idnumber = ?;
+            ", [$_GET['codigo_diario']]);
 
-        $notas = $DB->get_record_sql('
-            select   gi.idnumber codigo_nota, u.username matricula_aluno, gg.finalgrade nota_aluno
-            from     {grade_grades} gg
-                        inner join {grade_grades} gi on (gg.itemid=gi.id)
-                        inner join {grade_grades} c on (gi.courseid = c.id)
-                        inner join {user} u on (gg.userid=u.id)
-            where    gi.idnumber is not null and gi.idnumber != ''
-            and    c.idnumber = ?;
-        ', [$_GET['codigo_diario']]);
+            echo json_encode($notas);
+        } catch (Exception $ex) {
+            http_response_code(500);
+            if ($ex->getMessage() == "Data submitted is invalid (value: Data submitted is invalid)") {
+                echo json_encode(["error" => ["message" => "Ocorreu uma inconsistência no servidor do AVA. Este erro é conhecido e a solução dele já está sendo estudado pela equipe de desenvolvimento. Favor tentar novamente em 5 minutos."]]);
+            } else {
+                echo json_encode(["error" => ["message" => $ex->getMessage()]]);
+            }
+        }
     }
 
 }
@@ -32,4 +45,3 @@ class sync_up_enrolments_service extends service {
 
 $service = new sync_up_enrolments_service();
 $service->call();
-    
