acee8fba658fdbe58b85d28349e8fc5997754627
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2023-09-04 16:59:23 -0300

    feat: responde em formato JSON

--- api/index.php
+++ api/index.php
@@ -29,6 +29,7 @@ function exception_handler($exception) {
 
 try {
     require_once('../../../config.php');
+    header('Content-Type: application/json; charset=utf-8');
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
