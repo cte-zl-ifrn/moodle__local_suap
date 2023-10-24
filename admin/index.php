56f3ec11489e72106be77090ab4bdf5c8512ee9f
daqniwloe <danil0_braz@hotmail.com>
2023-10-24 20:18:50 -0300

    [UPD] Fix paginação no index

--- admin/index.php
+++ admin/index.php
@@ -16,28 +16,105 @@ if (!is_siteadmin()) {
 }
   
 echo $OUTPUT->header();
-$linhas = array_values($DB->get_records("suap_enrolment_to_sync", null, "id ASC", "id, timecreated, processed"));
-$statuses = [0=>"Não processado", 1=>"Sucesso", 2=>'Falha'];
-foreach ($linhas as $key => $value) {
+
+$ordenacao = isset($_GET['ordenacao']) ? $_GET['ordenacao'] : 'ASC';
+
+// Número de itens por página
+$itensPorPagina = 10;
+$paginaAtual = isset($_GET['pagina']) ? (int)$_GET['pagina'] : 1;
+
+// Consulta SQL personalizada para buscar os registros com LIMIT e OFFSET
+$sql = "
+    SELECT id, timecreated, processed
+    FROM {suap_enrolment_to_sync}
+    ORDER BY id $ordenacao, timecreated $ordenacao, processed $ordenacao
+    LIMIT :limit OFFSET :offset
+";
+
+$params = [
+    'limit' => $itensPorPagina,
+    'offset' => ($paginaAtual - 1) * $itensPorPagina,
+];
+$registros = $DB->get_records_sql($sql, $params);
+
+$statuses = [0 => "Não processado", 1 => "Sucesso", 2 => 'Falha'];
+foreach ($registros as $key => $value) {
     $value->status = $statuses[$value->processed];
 } 
 
-$totalRegistros = count($linhas); 
-$itensPorPagina = 10; 
+// Consulta SQL para contar o total de registros
+$sqlTotalRegistros = "SELECT COUNT(*) as total FROM {suap_enrolment_to_sync}" ;
 
-$paginaAtual = isset($_GET['pagina']) ? $_GET['pagina'] : 1;
+$totalRegistros = $DB->get_field_sql($sqlTotalRegistros);
 
-$totalPaginas = ceil($totalRegistros / $itensPorPagina);
+$numeroTotalDePaginas = ceil($totalRegistros / $itensPorPagina);
 
-$indiceInicio = ($paginaAtual - 1) * $itensPorPagina;
+$primeirasPaginas = 5;
+$ultimasPaginas = 3;
 
-$registrosPaginaAtual = array_slice($linhas, $indiceInicio, $itensPorPagina);
+$paginaInicio = max(2, $paginaAtual - floor($primeirasPaginas / 2));
+$paginaFim = $paginaInicio + $primeirasPaginas - 1;
+
+$registrosPaginaAtual = array_slice($registros, 0, $itensPorPagina);
+
+// verifica o numero total de páginas com o range de paginação, para delimitar um fim para a paginação, caso outras páginas sejam clicadas
+if ( in_array($numeroTotalDePaginas, range($paginaInicio, $paginaFim)) ) {
+    $primeirosCinco = range($paginaInicio, $numeroTotalDePaginas);
+}else{
+    $primeirosCinco = range($paginaInicio, $paginaFim);
+}
+
+$ultimosTres = range($numeroTotalDePaginas, $numeroTotalDePaginas);
+
+ $paginacaoVariada = [];
+
+// Verifica se tem mais de 13 páginas. Se tiver, irá acrescentar a lógica de aparecer as 3 ultimas.
+if ($numeroTotalDePaginas < $primeirasPaginas + $ultimasPaginas) {
+    $paginacaoVariada = range($paginaInicio, $paginaFim);
+    
+}else{
+
+    if($paginaAtual < $numeroTotalDePaginas-3 && $paginaAtual >= 5){
+        echo("TO AQUI");
+        $mergeUnique= array_unique(array_merge($primeirosCinco,['...'],$ultimosTres));      
+        $paginacaoVariada = array_merge(['...'], $mergeUnique);
+
+    }elseif($paginaAtual < $numeroTotalDePaginas-3){
+        $mergeUnique= array_unique(array_merge($primeirosCinco,['...'],$ultimosTres));       
+        $paginacaoVariada = array_merge($mergeUnique);
+
+    }elseif($paginaAtual >= 5){
+        $mergeUnique= array_unique(array_merge($primeirosCinco,$ultimosTres));        
+        $paginacaoVariada = array_merge(['...'], $mergeUnique);
+    }else{
+        $paginacaoVariada = array_unique(array_merge($primeirosCinco, $ultimosTres));
+
+    }
+    
+    // if($paginaAtual >= 5){
+    //     //  $paginacaoVariadaInicio = '...'; 
+    //     //  $mergePaginaPrimeirosCincoInicio[] = array_push($primeirosCinco, $paginacaoVariadaInicio);
+    //     // foreach($mergePaginaPrimeirosCincoInicio as $t){
+    //     //     echo($t.'-');
+    //     // }
+        
+    //     //  $paginacaoInicio= array_unique(array_merge($mergePaginaPrimeirosCincoInicio, $ultimosTres));
+    //     //  $paginacaoVariada = $mergePaginaPrimeirosCincoInicio; 
+    //     $mergeUnique= array_unique(array_merge($primeirosCinco,$ultimosTres));
+        
+
+        
+    //     $paginacaoVariada = array_merge(['...'], $mergeUnique);
+    //     echo("OPA");
+    // }
+    
+
+}
 
 $templatecontext = [
     'linhas' => $registrosPaginaAtual, 
-    'paginas' => range(1, $totalPaginas),
+    'paginas' => $paginacaoVariada,
 ];
-
 
 echo $OUTPUT->render_from_template('local_suap/index', $templatecontext);
 echo $OUTPUT->footer();
