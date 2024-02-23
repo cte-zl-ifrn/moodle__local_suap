7ca8b9ebf9e353f20f714e24cf67b2745c17446e
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2024-02-23 18:36:33 -0300

    feat: [add] tipo_usuario release: 20240223054

--- db/upgradelib.php
+++ db/upgradelib.php
@@ -29,30 +29,33 @@
 defined('MOODLE_INTERNAL') || die();
 
 require_once($CFG->dirroot . '/local/suap/locallib.php');
-function suap_save_course_custom_field($categoryid, $shortname, $name, $type='text', $configdata='{"required":"0","uniquevalues":"0","displaysize":50,"maxlength":250,"ispassword":"0","link":"","locked":"0","visibility":"0"}') {
+function suap_save_course_custom_field($categoryid, $shortname, $name, $type = 'text', $configdata = '{"required":"0","uniquevalues":"0","displaysize":50,"maxlength":250,"ispassword":"0","link":"","locked":"0","visibility":"0"}')
+{
     return \local_suap\get_or_create(
-        'customfield_field', 
-        ['shortname'=>$shortname], 
-        ['categoryid' => $categoryid, 'name' => $name, 'type' => $type, 'configdata' => $configdata, 'timecreated'=>time(), 'timemodified'=>time(), 'sortorder'=>\local_suap\get_last_sort_order('customfield_field')]
+        'customfield_field',
+        ['shortname' => $shortname],
+        ['categoryid' => $categoryid, 'name' => $name, 'type' => $type, 'configdata' => $configdata, 'timecreated' => time(), 'timemodified' => time(), 'sortorder' => \local_suap\get_last_sort_order('customfield_field')]
     );
 }
 
 
-function suap_save_user_custom_field($categoryid, $shortname, $name, $datatype='text', $visible=1, $p1=NULL, $p2=NULL) {
+function suap_save_user_custom_field($categoryid, $shortname, $name, $datatype = 'text', $visible = 1, $p1 = NULL, $p2 = NULL)
+{
     return \local_suap\get_or_create(
-        'user_info_field', 
-        ['shortname'=>$shortname], 
-        ['categoryid'=>$categoryid, 'name'=>$name, 'description' => $name, 'descriptionformat' => 2, 'datatype'=>$datatype, 'visible'=>$visible, 'param1'=>$p1, 'param2'=>$p2]
+        'user_info_field',
+        ['shortname' => $shortname],
+        ['categoryid' => $categoryid, 'name' => $name, 'description' => $name, 'descriptionformat' => 2, 'datatype' => $datatype, 'visible' => $visible, 'param1' => $p1, 'param2' => $p2]
     );
 }
 
 
-function suap_bulk_course_custom_field() {
+function suap_bulk_course_custom_field()
+{
     global $DB;
     $cid = \local_suap\get_or_create(
-        'customfield_category', 
-        ['name' => 'SUAP', 'component'=>'core_course', 'area'=>'course'], 
-        ['sortorder'=>\local_suap\get_last_sort_order('customfield_category'), 'itemid'=>0, 'contextid'=>1, 'descriptionformat'=>0, 'timecreated'=>time(), 'timemodified'=>time()]
+        'customfield_category',
+        ['name' => 'SUAP', 'component' => 'core_course', 'area' => 'course'],
+        ['sortorder' => \local_suap\get_last_sort_order('customfield_category'), 'itemid' => 0, 'contextid' => 1, 'descriptionformat' => 0, 'timecreated' => time(), 'timemodified' => time()]
     )->id;
     suap_save_course_custom_field($cid, 'campus_id', 'ID do campus');
     suap_save_course_custom_field($cid, 'campus_descricao', 'Descrição do campus');
@@ -83,10 +86,11 @@ function suap_bulk_course_custom_field() {
 }
 
 
-function suap_bulk_user_custom_field() {
+function suap_bulk_user_custom_field()
+{
     global $DB;
 
-    $cid = \local_suap\get_or_create('user_info_category', ['name' => 'SUAP'], ['sortorder'=>\local_suap\get_last_sort_order('user_info_category')])->id;
+    $cid = \local_suap\get_or_create('user_info_category', ['name' => 'SUAP'], ['sortorder' => \local_suap\get_last_sort_order('user_info_category')])->id;
 
     suap_save_user_custom_field($cid, 'email_google_classroom', 'E-mail @escolar (Google Classroom');
     suap_save_user_custom_field($cid, 'email_academico', 'E-mail @academico (Microsoft)');
@@ -102,22 +106,24 @@ function suap_bulk_user_custom_field() {
 
     suap_save_user_custom_field($cid, 'turma_id', 'ID da turma');
     suap_save_user_custom_field($cid, 'turma_codigo', 'Código da turma');
-    
+
     suap_save_user_custom_field($cid, 'polo_id', 'ID do pólo');
     suap_save_user_custom_field($cid, 'polo_nome', 'Nome do pólo');
-    
+
     suap_save_user_custom_field($cid, 'ingresso_periodo', 'Período de ingresso');
 
     suap_save_user_custom_field($cid, 'nome_apresentacao', 'Nome de apresentação');
     suap_save_user_custom_field($cid, 'nome_completo', 'Nome completo');
     suap_save_user_custom_field($cid, 'nome_social', 'Nome social');
+    suap_save_user_custom_field($cid, 'tipo_usuario', 'Tipo de usuário');
 
     suap_save_user_custom_field($cid, 'programa_nome', 'Nome do programa');
 
     suap_save_user_custom_field($cid, 'last_login', 'JSON do último login', 'textarea', 0);
 }
 
-function local_suap_migrate($oldversion) {
+function local_suap_migrate($oldversion)
+{
     global $DB;
 
     $dbman = $DB->get_manager();
@@ -131,7 +137,6 @@ function local_suap_migrate($oldversion) {
 
         $table->add_key("primary",      XMLDB_KEY_PRIMARY,  ["id"],         null,       null);
         $status = $dbman->create_table($table);
-
     }
     return true;
 }
