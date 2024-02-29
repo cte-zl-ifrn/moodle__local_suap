6365f888ad00a61959cbc57520b17ec484d76a3d
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2024-02-29 19:56:33 -0300

    feat: better health checker

--- healthcheck.php
+++ healthcheck.php
@@ -1,7 +1,19 @@
 <?php
 require_once("../../config.php");
-echo "<pre>PHP: OK.\n
-Configuração do Moodle: OK.\n
-Conexão com o banco: OK.\n
-Rota no .htaccess: OK.\n
-Tudo bem até aqui:" . " sim.\n";
+$pgversion = $DB->get_record_sql('SELECT version()');
+
+echo "<pre>\n";
+echo "\nHostname: " . gethostname();
+echo "\nPHP: " . phpversion();
+echo "\nMoodle: {$CFG->release}";
+echo "\nBanco: {$pgversion->version}";
+var_dump($CFG->dboptions);
+echo "\nWWW root: {$CFG->wwwroot}";
+echo "\nSession handler: {$CFG->session_handler_class}";
+echo "\nReverse proxy: {$CFG->reverseproxy}";
+echo "\nSSL proxy: {$CFG->sslproxy}";
+echo "\nCache JS: {$CFG->cachejs}";
+echo "\nCache Template: {$CFG->cachetemplates}";
+echo "\nCache Lang String: {$CFG->langstringcache}";
+echo "\nRota no .htaccess: OK";
+echo "\nTudo bem até aqu: sim.";
